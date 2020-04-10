df19ce6cafc29ba8c94511c0564ea5463f2d295b
Ilya-Trakhimovich <58522205+Ilya-Trakhimovich@users.noreply.github.com>
2020-04-10 15:07:07 +0300

    Add property SurName

--- ConsoleApp10/ExmpleGitHubDesktop.cs
+++ ConsoleApp10/ExmpleGitHubDesktop.cs
@@ -9,5 +9,6 @@ namespace ConsoleApp10
         public string Name { get; set; }
         public int Age { get; set;}
         public string PhoneNumber { get; set; }
+        public string SurName { get; set; }
     }
 }
