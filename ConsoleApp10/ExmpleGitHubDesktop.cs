4c1ccca1d5caa06d6d143501bf24d1030efb5c3f
Ilya-Trakhimovich <58522205+Ilya-Trakhimovich@users.noreply.github.com>
2020-04-10 14:59:08 +0300

    Create ExmpleGitHubDesktop.cs
    Create class ExmpleGitHubDesktop, add properties Name and Age

---
+++ ConsoleApp10/ExmpleGitHubDesktop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    public class ExmpleGitHubDesktop
+    {
+        public string Name { get; set; }
+        public int Age { get; set;}
+    }
+}
